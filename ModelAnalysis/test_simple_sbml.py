804b0ec1bb8fbcad7f5684297bf47704f54a7bc5
Joseph Hellerstein <jlheller@uw.edu>
2017-08-23 08:45:28 -0700

    Code fragments to develop model data extractor

---
+++ ModelAnalysis/test_simple_sbml.py
@@ -0,0 +1,38 @@
+"""
+Tests for simple_sbml
+"""
+import unittest
+import numpy as np
+from simple_sbml import SimpleSBML
+import libsbml
+
+
+IGNORE_TEST = False
+TEST_FILE = "chemotaxis.xml"
+NUM_REACTIONS = 111
+NUM_PARAMETERS = 27
+MAX_REACTANTS = 10
+
+
+#############################
+# Tests
+#############################
+class TestSimpleSBML(unittest.TestCase):
+
+  def setUp(self):
+    self.simple = SimpleSBML(TEST_FILE)
+
+  def testConstructor(self):
+    if IGNORE_TEST:
+      return
+    self.assertEqual(len(self.simple.getReactions()), NUM_REACTIONS)
+    for reaction in self.simple.getReactions():
+      self.assertTrue(isinstance(reaction, libsbml.Reaction))
+      self.assertLessEqual(reaction.getNumReactants(), MAX_REACTANTS)
+    self.assertEqual(len(self.simple.getParameters()), NUM_PARAMETERS)
+    for parameter in self.simple.getParameters():
+      self.assertTrue(isinstance(parameter, libsbml.Parameter))
+
+
+if __name__ == '__main__':
+  unittest.main()
