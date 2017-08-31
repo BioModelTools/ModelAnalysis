add340f37bf63adef816741ef5d66f85906cd86e
Joseph Hellerstein <jlheller@uw.edu>
2017-08-30 18:30:23 -0700

    Towards full pattern analyzer.

---
+++ ModelAnalysis/pattern_analyzer.py
@@ -0,0 +1,4 @@
+""" Analyzes patterns in BioModels, putting the statistics in a CSV."""
+
+# Input is stdin with BioModels IDs. Outputs to to std out.
+# May want something else as output since stdout may be for status
